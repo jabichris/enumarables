905d010b57dfc0dd7e3f88ebd88e772866fd150b
jabichris <seannas04@gmail.com>
2020-03-16 18:32:49 +0200

    fix errors

--- enumarables.rb
+++ enumarables.rb
@@ -1,33 +1,33 @@
 module Enumerable
   def my_each
-        return to_enum(:my_each) unless block_given? 
-        index = 0
-        while index < size
-          yield(self[index])
-          index += 1
-        end
-        self
+    return to_enum(:my_each) unless block_given?
+
+    index = 0
+    while index < size
+      yield(self[index])
+      index += 1
+    end
+    self
   end
 
   def my_each_with_index(given_index = nil)
-        return enum_for(:my_each_with_index) unless block_given?
-    
-        index = 0
-        index = given_index unless given_index.nil?
-        my_each do |x|
-          yield(x, index)
-          index += 1
-        end
-        self
-  end
-    
-    
-  def my_select
-      select_array = []
-      return enum_for(:my_select) unless block_given?
+    return enum_for(:my_each_with_index) unless block_given?
 
-      my_each { |x| select_array << x if yield x }
-      select_array
+    index = 0
+    index = given_index unless given_index.nil?
+    my_each do |x|
+      yield(x, index)
+      index += 1
+    end
+    self
+  end
+
+  def my_select
+    select_array = []
+    return enum_for(:my_select) unless block_given?
+
+    my_each { |x| select_array << x if yield x }
+    select_array
   end
 
   def my_all?(pattern = nil)
@@ -85,6 +85,7 @@ module Enumerable
 
   def my_map(proc = nil)
     return enum_for(:my_map) unless block_given?
+
     arr = []
     each do |x|
       arr << if block_given?
@@ -92,72 +93,72 @@ module Enumerable
              else
                proc.call(x)
              end
-        end
+    end
     arr
-   end
   end
+end
 
-  def my_inject(accumulator = nil, value_given = nil)
-    return symbol_logic(value_given, accumulator) if accumulator.class == Integer && !value_given.nil?
-    return symbol_logic(accumulator, 0) if accumulator.class == Symbol
-    each do |x|
-      accumulator = if accumulator.class == Integer
-                      yield(accumulator, x)
-                    elsif accumulator.nil?
-                      x
-                    else
-                      yield(accumulator, x)
-                    end
-     end
-    accumulator
+def my_inject(accumulator = nil, value_given = nil)
+  return symbol_logic(value_given, accumulator) if accumulator.class == Integer && !value_given.nil?
+  return symbol_logic(accumulator, 0) if accumulator.class == Symbol
+
+  each do |x|
+    accumulator = if accumulator.class == Integer
+                    yield(accumulator, x)
+                  elsif accumulator.nil?
+                    x
+                  else
+                    yield(accumulator, x)
+                  end
   end
+  accumulator
+end
 
-  def symbol_logic(symbol, accumulator)
-    each do |x|
-      case symbol
-      when :+
-        accumulator += x
-      when :-
-        accumulator -= x
-      when :*
-        accumulator *= x
-      when :/
-        accumulator /= x
-      end
+def symbol_logic(symbol, accumulator)
+  each do |x|
+    case symbol
+    when :+
+      accumulator += x
+    when :-
+      accumulator -= x
+    when :*
+      accumulator *= x
+    when :/
+      accumulator /= x
     end
-    accumulator
   end
+  accumulator
+end
 
-  def all_check(pattern, exponent)
-    if pattern.is_a? Regexp
-      return false if exponent !~ pattern
-    elsif pattern.is_a? Class
-      return false unless exponent.is_a? pattern
-    elsif exponent != pattern
-      return false
-    end
-    true
+def all_check(pattern, exponent)
+  if pattern.is_a? Regexp
+    return false if exponent !~ pattern
+  elsif pattern.is_a? Class
+    return false unless exponent.is_a? pattern
+  elsif exponent != pattern
+    return false
   end
+  true
+end
 
-  def any_check(pattern, exponent)
-    if pattern.is_a? Regexp
-      return false if pattern =~ exponent
-    elsif pattern.is_a? Class
-      return true if exponent.is_a? pattern
-    elsif exponent == pattern
-      return true
-    end
-    false
+def any_check(pattern, exponent)
+  if pattern.is_a? Regexp
+    return false if pattern =~ exponent
+  elsif pattern.is_a? Class
+    return true if exponent.is_a? pattern
+  elsif exponent == pattern
+    return true
   end
+  false
+end
 
-  def none_check(pattern, exponent)
-    if pattern.is_a? Regexp
-      return false if pattern =~ exponent
-    elsif pattern.is_a? Class
-      return false if exponent.is_a? pattern
-    elsif exponent == pattern
-      return false
-    end
-    true
+def none_check(pattern, exponent)
+  if pattern.is_a? Regexp
+    return false if pattern =~ exponent
+  elsif pattern.is_a? Class
+    return false if exponent.is_a? pattern
+  elsif exponent == pattern
+    return false
   end
-  
+  true
+end
